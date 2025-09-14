5f3fe1b6029069b7e8df335c65f1e06bf73b0e50
Vagner Araújo <gnerprocs@gmail.com>
2025-09-14 16:49:01 -0300

    iniciando catalog api & docker compose

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IcommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+        where TCommand : ICommand
+    {
+    }
+
+    public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+        where TQuery : IQuery<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
